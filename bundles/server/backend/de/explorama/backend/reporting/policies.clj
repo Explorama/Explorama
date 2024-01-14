a799c3d2820f304e54efbbcf4833a7cedf74419b
Sören Stöhrmann <soeren.stoehrmann@gmail.com>
2024-01-14 15:29:37 +0100

    Cleaning up server and preparing single user server

--- bundles/server/backend/de/explorama/backend/reporting/policies.clj
+++
@@ -1,6 +0,0 @@
-(ns de.explorama.backend.reporting.policies)
-
-(def standard-rights {:rights-and-roles {:user-attributes {:role ["admin"]}
-                                         :data-attributes {}
-                                         :id :rights-and-roles
-                                         :name "reporting-rights-and-roles"}})

2887dab4274e62d3e9bc61a9e6079cf99af7222a
JPK <2knu@users.noreply.github.com>
2024-02-08 22:37:47 +0100

    Fix #24: map filter not working. Reason: Async-callback was not implemented

--- bundles/electron/backend/de/explorama/backend/frontend_api.cljs
+++ bundles/electron/backend/de/explorama/backend/frontend_api.cljs
@@ -30,7 +30,7 @@
   (dispatch event))
 
 (defn- route-wrapper [route-fn [_ metas & params]]
-  (let [{:keys [client-callback failed-callback user-info client-id broadcast-callback broadcast-filter custom]}
+  (let [{:keys [client-callback failed-callback async-callback user-info client-id broadcast-callback broadcast-filter custom]}
         (when (map? metas) metas)
         ignore-first? (and (map? metas)
                            (or client-callback broadcast-callback user-info client-id))
@@ -54,6 +54,9 @@
                        :client-callback (fn [& params]
                                           (when client-callback
                                             (dispatch (apply conj client-callback params))))
+                       :async-callback (fn [& params]
+                                         (when async-callback
+                                           (dispatch (apply conj async-callback params))))
                        :user-validation (constantly true)}
                 (map? custom)
                 (into (map (fn [[k event]]

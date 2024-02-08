69b3000c57a52cb807d22b476a77e624804b817d
JPK <2knu@users.noreply.github.com>
2024-02-08 21:28:42 +0100

    bump clojurescript to 1.11.132

--- bundles/electron/project.clj
+++ bundles/electron/project.clj
@@ -103,7 +103,7 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.11.60"]
+                 [org.clojure/clojurescript "1.11.132"]
                  [org.clojure/core.async "1.6.681"]
                  ;;  [metosin/jsonista "0.3.7"]
                  ;for electron

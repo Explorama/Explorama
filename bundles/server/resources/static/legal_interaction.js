4b4a2c963477617e9d400a29df29a9a29606a6f6
Sören Stöhrmann <soeren.stoehrmann@gmail.com>
2025-01-19 15:43:44 +0100

    Refactoring folders

--- bundles/server/resources/static/legal_interaction.js
+++
@@ -1,9 +0,0 @@
-function hideLegal(legalID) {
-    let element = document.getElementById(legalID);
-    element.classList.add("hidden-legal");
-}
-
-function showLegal(legalID) {
-    let element = document.getElementById(legalID);
-    element.classList.remove("hidden-legal");
-}

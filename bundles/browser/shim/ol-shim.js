d2dd3b1fd3feebfef65f177223e290fa80bc72d3
Soeren Stoehrmann <soeren.stoehrmann@gmail.com>
2026-01-02 10:59:57 +0100

    remove ai comments

--- bundles/browser/shim/ol-shim.js
+++ bundles/browser/shim/ol-shim.js
@@ -1,33 +1,31 @@
-import * as ol from 'ol';
-import * as olColor from 'ol/color.js';
-import * as olControl from 'ol/control.js';
-import * as olCoordinate from 'ol/coordinate.js'; // Import coordinate
-import * as olEasing from 'ol/easing.js';
-import * as olEvents from 'ol/events.js';
-import * as olExtent from 'ol/extent.js';
-import * as olFormat from 'ol/format.js';
-import * as olGeom from 'ol/geom.js';
-import * as olHas from 'ol/has.js';
-import * as olInteraction from 'ol/interaction.js';
-import * as olLayer from 'ol/layer.js';
-import BaseLayer from 'ol/layer/Base.js';
-import * as olLoadingstrategy from 'ol/loadingstrategy.js';
-import * as olProj from 'ol/proj.js';
-import * as olRender from 'ol/render.js';
-import * as olSource from 'ol/source.js';
-import * as olSphere from 'ol/sphere.js';
-import * as olStyle from 'ol/style.js';
-import * as olTilegrid from 'ol/tilegrid.js';
-import * as olUtil from 'ol/util.js';
-import View from 'ol/View.js';
+import * as ol from "ol";
+import * as olColor from "ol/color.js";
+import * as olControl from "ol/control.js";
+import * as olCoordinate from "ol/coordinate.js";
+import * as olEasing from "ol/easing.js";
+import * as olEvents from "ol/events.js";
+import * as olExtent from "ol/extent.js";
+import * as olFormat from "ol/format.js";
+import * as olGeom from "ol/geom.js";
+import * as olHas from "ol/has.js";
+import * as olInteraction from "ol/interaction.js";
+import * as olLayer from "ol/layer.js";
+import BaseLayer from "ol/layer/Base.js";
+import * as olLoadingstrategy from "ol/loadingstrategy.js";
+import * as olProj from "ol/proj.js";
+import * as olRender from "ol/render.js";
+import * as olSource from "ol/source.js";
+import * as olSphere from "ol/sphere.js";
+import * as olStyle from "ol/style.js";
+import * as olTilegrid from "ol/tilegrid.js";
+import * as olUtil from "ol/util.js";
+import View from "ol/View.js";
 
-// Create a mutable object extending the base ol namespace
 const olGlobal = Object.assign({}, ol);
 
-// Attach missing modules
 olGlobal.color = olColor;
 olGlobal.control = olControl;
-olGlobal.coordinate = olCoordinate; // Attach coordinate
+olGlobal.coordinate = olCoordinate;
 olGlobal.easing = olEasing;
 olGlobal.events = olEvents;
 olGlobal.extent = olExtent;
@@ -36,7 +34,6 @@ olGlobal.geom = olGeom;
 olGlobal.has = olHas;
 olGlobal.interaction = olInteraction;
 
-// Handle ol.layer
 const mutableLayer = Object.assign({}, olLayer);
 mutableLayer.Base = BaseLayer;
 olGlobal.layer = mutableLayer;
@@ -52,11 +49,12 @@ olGlobal.tilegrid = olTilegrid;
 olGlobal.util = olUtil;
 
 if (!olGlobal.View) {
-    olGlobal.View = View;
+  olGlobal.View = View;
 }
 
-if (typeof window !== 'undefined') {
-    window.ol = olGlobal;
+if (typeof window !== "undefined") {
+  window.ol = olGlobal;
 }
 
 export default olGlobal;
+

0152301b66a3400588d71ad899c4be41d7e503ea
Soeren Stoehrmann <soeren.stoehrmann@gmail.com>
2026-01-10 17:42:30 +0100

    testing

--- bundles/server/run-tests-ci.bb.clj
+++ bundles/server/run-tests-ci.bb.clj
@@ -1,13 +1,13 @@
 (require '[babashka.process :refer [shell]]
          '[clojure.string :as str])
 
-(loop [lines (-> (shell {:out :string} "clj -M:test-ci")
+(loop [lines (-> (shell {:out :string} "clj -Sdeps cljs.deps.edn -M:test-ci")
                  :out
                  (str/split #"\n"))
        result ""
        toggle false]
   (if (empty? lines)
-    (spit "report.xml" result :encoding "UTF-8")
+    (spit "client-report.xml" result :encoding "UTF-8")
     (cond (= "### report start ###" (first lines))
           (recur (rest lines) result true)
           (= "### report end ###" (first lines))
